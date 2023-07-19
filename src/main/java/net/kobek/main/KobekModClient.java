eeed863f4e0296c36d11e47b8306532859b8057b
kobek <123257619+kobekma@users.noreply.github.com>
2023-07-19 23:15:26 +0200

    Initial commit

---
+++ src/main/java/net/kobek/main/KobekModClient.java
@@ -0,0 +1,11 @@
+package net.kobek.main;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KobekModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
